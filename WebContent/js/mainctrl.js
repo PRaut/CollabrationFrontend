edce961ef9ef64752ca8e1023ce97a86f6e02210
Prathamesh Raut <prathamesh.raut001@gmail.com>
2018-04-01 19:48:05 +0530

    Forum menu added and angular routes added - Done

--- WebContent/js/mainctrl.js
+++ WebContent/js/mainctrl.js
@@ -6,10 +6,16 @@ myApp.config(function($routeProvider) {
 	$routeProvider.when("/", {templateUrl : "template/Home.html"})
 		.when("/home", {templateUrl : "template/Home.html" })
 		.when("/about", { templateUrl : "template/AboutUs.html" })
-		.when("/blog", { templateUrl : "c_blog/blog.html" })
-		.when("/manageBlogs", {templateUrl: "c_blog/manageBlogs.html"})
-		.when("/updateBlog", {templateUrl: "c_blog/updateBlog.html"})
 		.when("/login", { templateUrl : "template/Login.html" })
 		.when("/signup", { templateUrl : "template/SignUp.html" })
 		.when("/contactus", { templateUrl : "template/ContactUs.html"})
+		
+		// Blogs
+		.when("/blog", { templateUrl : "c_blog/blog.html" })
+		.when("/manageBlogs", {templateUrl: "c_blog/manageBlogs.html"})
+		.when("/updateBlog", {templateUrl: "c_blog/updateBlog.html"})
+		
+		// forum
+		.when("/createForum", {templateUrl: "c_forum/createForum.html"})
+		.when("/manageForums", {templateUrl: "c_forum/manageForums.html"})
 });
