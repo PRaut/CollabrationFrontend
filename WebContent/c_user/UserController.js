cc37ac35b3fd1f92d47dea5e55caee686b95b602
Prathamesh Raut <prathamesh.raut001@gmail.com>
2018-04-21 17:01:15 +0530

    Profile Image upload done but issue in redirect after upload

---
+++ WebContent/c_user/UserController.js
@@ -0,0 +1,42 @@
+myApp.controller("UserController", function($scope, $http, $location, $rootScope, $cookieStore){
+	
+	$scope.user = {
+			"userName": '',
+			"password": '',
+			"email": '',
+			"phone": '',
+			"address": '',
+			"accountOpeningDate": '',
+			"role": '',
+			"isOnline": ''
+	};
+	
+	$rootScope.login = function(){
+		console.log('inside login()');
+		
+		$http.post('http://localhost:8082/ChatterMiddleware/login', $scope.user)
+		.then(function(response){
+			console.log('Login status '+ response.statusText);
+			$scope.user = response.data;
+			$rootScope.currentUser = response.data;
+			$cookieStore.put('userDetails', response.data);
+			console.log('Current user Role'+ $rootScope.currentUser.role);
+			console.log($rootScope.currentUser.userName);
+			/*if($rootScope.currentUser.role == 'ADMIN'){
+				console.log('Role Found ADMIN' );
+				//console.log('ADMIN PAGE');
+			}else{
+				console.log('Role found USER');
+			}*/
+			$location.path("/home");
+		});
+	};
+	
+	$rootScope.logout=function()
+	{
+		console.log('Logout Function');
+		delete $rootScope.currentUser;
+		$cookieStore.remove('userDetails');
+		$location.path("/home");
+	}
+});
